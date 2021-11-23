38549417d2efc63d5ec4837971403f43f5c61fef
BCorbin825 <73535716+BCorbin825@users.noreply.github.com>
2021-11-23 00:11:28 -0500

    Add files via upload

--- Graph.h
+++ Graph.h
@@ -2,45 +2,48 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <climits>
+#include <vector>
+#include <set>
 
 using namespace std;
 
 class Vertex { // Define a vertex
-    private:
-        string city;
-        bool inPath, selected; // Used for printing vertices to window
+private:
+    string city;
+    bool inPath, selected; // Used for printing vertices to window
 
-    public:
-        // Constructors
-        Vertex() : city("Roadtrip"), inPath(false), selected(false) {}
-        Vertex(string City) : city(City), inPath(false), selected(false) {}
+public:
+    // Constructors
+    Vertex() : city("Roadtrip"), inPath(false), selected(false) {}
+    Vertex(string City) : city(City), inPath(false), selected(false) {}
 
-        // Getter functions 
-        string& GetName() { return city; }
-        bool& GetPathState() { return inPath; }
-        bool& GetSelectedState() { return selected; }
+    // Getter functions 
+    string& GetName() { return city; }
+    bool& GetPathState() { return inPath; }
+    bool& GetSelectedState() { return selected; }
 };
 
 class Graph {
-    private:
-        // Define graph structure
-        unordered_map<string, pair<Vertex, unordered_map<string, pair<Vertex, int>>>> adjList;
+private:
+    // Define graph structure
+    unordered_map<string, pair<Vertex, unordered_map<string, pair<Vertex, int>>>> adjList;
 
-    public:
-        // Constructor 
-        Graph(){}
-        
-        // Insert functions
-        void InsertEdge(string from, string to, int weight); // Insert edge between vertices (cities)
-        void Insert(string city); // Insert new vertex (city) into the graph
+public:
+    // Constructor 
+    Graph() {}
 
-        // Getter functions
-        int GetWeight(string from, string to); // Get weight between to vertices (miles between cities)
-        vector<string> GetAdjacent(string city); // Get all vertices directly connected to the given vertex
-        unordered_map& GetGraph() { return adjList; }
+    // Insert functions
+    void InsertEdge(string from, string to, int weight); // Insert edge between vertices (cities)
+    void Insert(string city); // Insert new vertex (city) into the graph
 
-        // Print function
-        void PrintGraph(); // Print all vertices
+    // Getter functions
+    int GetWeight(string from, string to); // Get weight between to vertices (miles between cities)
+    vector<string> GetAdjacent(string city); // Get all vertices directly connected to the given vertex
+    //unordered_map& GetGraph() { return adjList; }
+
+    // Print function
+    void PrintGraph(); // Print all vertices
 };
 
 void Graph::InsertEdge(string from, string to, int weight) {    // Insert edge between vertices (cities)
@@ -58,7 +61,7 @@ int Graph::GetWeight(string from, string to) {  // Get weight between to vertice
 vector<string> Graph::GetAdjacent(string city) {  // Get all vertices directly connected to the given vertex
     vector<string> adjacent;
     for (auto i : adjList[city].second) { // Iterate through adjacent vertices
-        adjacent.push_back(i.second.first.GetName()); 
+        adjacent.push_back(i.second.first.GetName());
     }
     return adjacent;
 }
@@ -67,4 +70,39 @@ void Graph::PrintGraph() {  // Print all vertices
     for (auto i : adjList) { // Iterate through graph
         cout << i.first << endl; // Print city names
     }
+}
+
+vector<int> dijkstra(const Graph& graph, int src) {
+    vector<int> parent(graph.numVertices, -1);
+    vector<int> distance(graph.numVertices, INT_MAX);
+    set<int> visited;
+    set<int> notVisited;
+
+    for (int i = 0; i < graph.numVertices; i++) {
+        notVisited.insert(i);
+    }
+    int current = src;
+    distance[src] = 0;
+    while (visited.size() != graph.numVertices) {
+        for (auto iter : graph.adjList[current]) {
+            int to = iter.first;
+            int newDistance = distance[current] + iter.second;
+            if (newDistance < distance[to]) {
+                distance[to] = newDistance;
+                parent[to] = current;
+            }
+        }
+
+        visited.insert(current);
+        notVisited.erase(current);
+        int lowestDistance = INT_MAX;
+
+        for (auto iter : notVisited) {
+            if (distance[iter] < lowestDistance) {
+                current = iter;
+                lowestDistance = distance[iter];
+            }
+        }
+    }
+    return distance;
 }
