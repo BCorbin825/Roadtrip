28cf38d0ae4c49ede8819ec2f65a3cb044942459
BCorbin825 <73535716+BCorbin825@users.noreply.github.com>
2021-12-09 17:38:07 -0500

    Add files via upload

--- Graphics.h
+++ Graphics.h
@@ -2,63 +2,50 @@
 #include <SFML/Graphics.hpp>
 #include <string>
 #include <unordered_map>
-#include <fstream>
-#include <sstream>
 #include "Graph.h"
 
 using namespace std;
 using namespace sf;
 
-void LoadImages(unordered_map<string, Texture>& textures, unordered_map<string, Sprite>& sprites) {
-    // Load all required textures from file
-    textures["map"].loadFromFile("Images/US2.png");
-    textures["inPath"].loadFromFile("Images/dot.png");
-    textures["ping"].loadFromFile("Images/MapIconsTransparent.png");
+void LoadImages(unordered_map<string, Texture>& textures, unordered_map<string, Sprite>& sprites) { // Load all relevant textures and sprites 
+	// Load textures from file
+	textures["map"].loadFromFile("Images/US4.png");
+	textures["inPath"].loadFromFile("Images/dot.png");
+	textures["ping"].loadFromFile("Images/MapIconsTransparent.png", IntRect(83, 3, 24, 40));
+	textures["menu"].loadFromFile("Images/Menu2.png");
+	textures["menu_closed"].loadFromFile("Images/MenuIconClosed.png");
+	textures["menu_open"].loadFromFile("Images/MenuIconOpen.png");
+	textures["selection"].loadFromFile("Images/Selection.png");
+	textures["loadscreen"].loadFromFile("Images/LoadScreen.png");
+	textures["line"].loadFromFile("Images/Line.png");
+	textures["textbar"].loadFromFile("Images/TextBar.png");
 
-    // Map sprite
-    sprites["map"].setTexture(textures["map"]);
+	// Map sprite
+	sprites["map"].setTexture(textures["map"]);
 
-    // In path sprite
-    sprites["inPath"].setTexture(textures["inPath"]);
-    sprites["inPath"].setPosition(150, 100);
+	// In path sprite
+	sprites["inPath"].setTexture(textures["inPath"]);
 
-    // Ping sprite
-    sprites["ping"].setTexture(textures["ping"]);
-    sprites["ping"].setTextureRect(IntRect(83, 3, 24, 40));
-    sprites["ping"].setPosition(100, 100);
+	// Ping sprite
+	sprites["ping"].setTexture(textures["ping"]);
+
+	// Menu icon sprite
+	sprites["menu"].setTexture(textures["menu"]);
+	sprites["menu"].setPosition(1335, 52);
+	sprites["menu_closed"].setTexture(textures["menu_closed"]);
+	sprites["menu_closed"].setPosition(1550, 0);
+	sprites["menu_open"].setTexture(textures["menu_open"]);
+	sprites["menu_open"].setPosition(1550, 0);
+	sprites["selection"].setTexture(textures["selection"]);
+	sprites["selection"].setPosition(1349, 68);
+	sprites["textbar"].setTexture(textures["textbar"]);
+	sprites["textbar"].setPosition(1356, 177);
+
+	// Load screen
+	sprites["loadscreen"].setTexture(textures["loadscreen"]);
+	sprites["line"].setTexture(textures["line"]);
+	sprites["line"].setPosition(332, 454);
+	sprites["line"].setTextureRect(IntRect(0, 0, 0, 24));
 }
 
 
-// Adapted from COP3502 lab 7
-void LoadData(string file, Graph& graph, unordered_map<string, Texture>& textures, unordered_map<string, Sprite>& sprites) { //Loads a file
-
-	ifstream inFile(file);	//Input file stream
-
-	if (inFile.is_open()) { //Checks if the file is open
-
-		string line;
-		getline(inFile, line);
-
-		while (getline(inFile, line)) { //Loops through CSV file 
-
-			istringstream stream(line);
-
-			string city, x, y;
-			int xPos, yPos;
-
-			//Get information from file for each variable 
-			getline(stream, city, ',');
-			getline(stream, x, ',');
-			getline(stream, y, ',');
-
-			//Converts to appropriate data type
-			xPos = stoi(x);
-			yPos = stoi(y);
-
-			graph.GetGraph()[city].first.GetName() = city;
-			textures[city].loadFromFile("Images/" + city + ".png");
-			sprites[city].setTexture(textures[city]);
-			sprites[city].setPosition(xPos, yPos);
-		}
-	}
-}
