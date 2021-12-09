e43367a2802944735d708bfd53aa7fe0b7899540
BCorbin825 <73535716+BCorbin825@users.noreply.github.com>
2021-12-09 17:38:18 -0500

    Add files via upload

--- Roadtrip.cpp
+++ Roadtrip.cpp
@@ -3,31 +3,40 @@
 #include <unordered_map>
 #include "Graph.h"
 #include "Graphics.h"
+#include "EventCheck.h"
+#include <sstream>
+#include <fstream>
 
 using namespace std;
 using namespace sf;
 
 
 int main() {
-    RenderWindow window(VideoMode(1600, 1000), "Roadtrip")
+    RenderWindow window(VideoMode(1600, 870), "Roadtrip");
     Graph graph;
+    Font font;
+    if (!font.loadFromFile("Dependencies/times_new_roman.ttf")) {
+        cout << "Font not loaded" << endl;
+        return -1;
+    }
+    WindowEvent wEvent = WindowEvent(font);
     unordered_map<string, Texture> textures;
     unordered_map<string, Sprite> sprites;
-    LoadImages(textures, sprites);
-    // added comment
+
+    LoadImages(textures, sprites); // Load sprites and textures
+    graph.LoadData("Dependencies/adjList.txt", window, sprites); // Create graph structure
+
     while (window.isOpen()) {
         Event event;
         while (window.pollEvent(event))
         {
-            //CheckEvent(window);
+            wEvent.CheckEvent(window, event, graph); // Update all relevant code based on event type
             if (event.type == Event::Closed)
                 window.close();
         }
 
         window.clear(Color::White);
-        window.draw(sprites["map"]);
-        window.draw(sprites["ping"]);
-        window.draw(sprites["inPath"]);
+        wEvent.UpdateWindow(window, graph, sprites); // Print current program state to window
         window.display();
     }
     return 0;
